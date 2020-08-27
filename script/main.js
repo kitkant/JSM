2ab1390729d06aaec9affa9bf2b5ae2ce2507122
Nick <nikitadyshkant@gmail.com>
2020-08-27 21:48:57 +0300

    homework5

--- script/main.js
+++ script/main.js
@@ -1,7 +1,9 @@
 const btnKick = document.getElementById('btn-kick')
 const btnBallLightning = document.getElementById('btn-ball_lightning')
+const logs = document.querySelector('#logs')
 
-let fatality = 0
+let fatality = 0,
+    countRound = 0
 
 
 const character = {
@@ -38,6 +40,25 @@ btnBallLightning.addEventListener('click', () => {
     enemy.changeHP(50,  fatality)
 })
 
+function generateLog(firstPerson, secondPerson, count, HP) {
+    const logs = [
+        `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. ${count} [${HP} / 100]`,
+        `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. ${count} [${HP} / 100]`,
+        `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. ${count} [${HP} / 100]`,
+        `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. ${count} [${HP} / 100]`,
+        `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. ${count} [${HP} / 100]`,
+        `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. ${count} [${HP} / 100]`,
+        `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. ${count} [${HP} / 100]`,
+        `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника ${count} [${HP} / 100]`,
+        `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. ${count} [${HP} / 100]`,
+        `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. ${count} [${HP} / 100]`
+    ];
+
+
+    return logs[random(logs.length) - 1]
+}
+
+
 function init(enemy, character) {
 
     character.renderHP()
@@ -61,8 +82,9 @@ function renderProgressbarHP(){
     this.elProgressbar.style.width = this.damageHP + '%'
 }
 function changeHP(count, fatality = 0){
+    countRound++
 
-   if(fatality === 1){
+   if(fatality === 1 || this.damageHP <= count){
        btnBallLightning.disabled = true
    }
     if(this.damageHP <= count){
@@ -75,6 +97,11 @@ function changeHP(count, fatality = 0){
    }
 
     this.renderHP()
+    const log = this === enemy ?  generateLog(this, character, count, this.damageHP) : generateLog( this, enemy, count, this.damageHP)
+    const p = document.createElement('p')
+    p.innerText = `Удар ${countRound}: ${log}`
+    logs.insertBefore(p, logs.children[0])
+
 }
 
 function random(num){
